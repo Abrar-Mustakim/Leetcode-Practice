d8d986cf8545ef302c74c19d04d847eef5482238
S.M.ABRAR MUSTAKIM TAKI <79537159+Abrar-Mustakim@users.noreply.github.com>
2021-09-08 17:14:21 +0600

    Update reverse linked list.py

--- reverse linked list.py
+++ reverse linked list.py
@@ -23,3 +23,47 @@ class Solution:
             temp1 = new 
             
         return temp
+    
+    
+    
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def removeElements(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        
+        
+        if head is None:
+            return head 
+        
+        
+        
+        while head is not None:
+            if head.val == val:
+                head = head.next 
+                
+                
+                
+            else:
+                break 
+                
+        temp = head
+                
+        while temp is not None and temp.next is not None:
+            
+            if temp.next.val == val:
+                new = temp.next.next 
+                
+                temp.next = new
+                
+            else:
+                temp = temp.next
+            
+            
+        
+        return head
+        
+        
+             
