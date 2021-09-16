346916afcbe2f8632f7f5038d907df2aa375a7c7
S.M.ABRAR MUSTAKIM TAKI <79537159+Abrar-Mustakim@users.noreply.github.com>
2021-09-16 15:07:14 +0600

    Create post order traversal.py

---
+++ post order traversal.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    
+    def __init__(self):
+        self.order = []
+    
+    
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        if root is None:
+            return 
+        
+        self.postorderTraversal(root.left)
+        self.postorderTraversal(root.right)
+        self.order.append(root.val)
+        
+        return self.order
