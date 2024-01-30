1cdfec7ddf53f5bd118b973a6f81e6feea578f64
S.M.ABRAR MUSTAKIM TAKI <79537159+Abrar-Mustakim@users.noreply.github.com>
2024-01-30 21:03:37 +0600

    Update set_matrix_zeroes.py

--- set_matrix_zeroes.py
+++ set_matrix_zeroes.py
@@ -20,4 +20,42 @@ class Solution:
                 matrix[i][j] = 0
 
         return matrix
+
+
+#2nd Method
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = [0] * len(matrix)
+        n = [0] * len(matrix[0])
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if matrix[i][j] == 0:
+                    m[i] = 1 
+                    n[j] = 1
+        for i in range(len(m)):
+            for j in range(len(n)):
+                if m[i] or n[j]:
+                    matrix[i][j] = 0
+        return matrix
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
