07500fc11913aefc01274893e69e120bb71fd233
S.M.ABRAR MUSTAKIM TAKI <79537159+Abrar-Mustakim@users.noreply.github.com>
2024-04-08 03:27:14 +0600

    Add files via upload

---
+++ find minimum in rotated sorted array.cpp
@@ -0,0 +1,34 @@
+//https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/?envType=study-plan-v2&envId=binary-search
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int low=0;
+        int high = nums.size()-1;
+        int ans=INT_MAX;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            //left sorted
+            if(nums[low]<=nums[mid]){
+
+                if(nums[low]<ans){
+                    ans=nums[low];
+                }
+                low = mid+1;
+
+
+            }
+
+            //right sorted
+            else{
+                if(nums[mid]<ans){
+                    ans=nums[mid];
+                }
+                high = mid-1;
+
+            }
+        }
+        return ans;
+    }
+};
